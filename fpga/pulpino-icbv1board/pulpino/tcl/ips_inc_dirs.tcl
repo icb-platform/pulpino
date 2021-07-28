e8d9158c30ec062b8aed02b0a4785ed1cf1fbe77
wanjun <you@example.com>
2021-07-28 19:59:51 +0800

    temp add

---
+++ fpga/pulpino-icbv1board/pulpino/tcl/ips_inc_dirs.tcl
@@ -0,0 +1,13 @@
+set_property include_dirs {
+    ../../.././rtl/includes \
+    ../../.././ips/axi/axi_node/. \
+    ../../.././ips/apb/apb_event_unit/./include/ \
+    ../../.././ips/fpu/. \
+    ../../.././ips/apb/apb_i2c/. \
+    ../../.././ips/ibex/include \
+    ../../.././ips/ibex/include \
+    
+    ../../.././ips/riscv/../../rtl/includes \
+ 
+    ../../.././ips/adv_dbg_if/rtl \
+} [current_fileset] 
