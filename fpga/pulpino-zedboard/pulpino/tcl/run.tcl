f34a5942733f438a4be0a79e64aeb0ac249b5bc7
wanjun <you@example.com>
2021-07-28 17:36:06 +0800

    fpga_build_script_update

--- fpga/pulpino-zedboard/pulpino/tcl/run.tcl
+++ fpga/pulpino-zedboard/pulpino/tcl/run.tcl
@@ -10,50 +10,7 @@ if { ![info exists ::env(XILINX_BOARD)] } {
   set ::env(XILINX_BOARD) "em.avnet.com:zynq:zed:c"
 }
 
-if { ![info exists ::env(USE_ZERO_RISCY)] } {
-  puts "USE_ZERO_RISCY = 0"
-  set ::env(USE_ZERO_RISCY) 0
-} else {
-  puts "USE_ZERO_RISCY = 1"
-}
-if { ![info exists ::env(RISCY_RV32F)] } {
-  puts "RISCY_RV32F = 0"
-  set ::env(RISCY_RV32F) 0
-} else {
-  puts "RISCY_RV32F = 1"
-}
-if { ![info exists ::env(ZERO_RV32M)] } {
-  puts "ZERO_RV32M = 0"
-  set ::env(ZERO_RV32M) 0
-} else {
-  puts "ZERO_RV32M = 1"
-}
-if { ![info exists ::env(ZERO_RV32E)] } {
-  puts "ZERO_RV32E = 0"
-  set ::env(ZERO_RV32E) 0
-} else {
-  puts "ZERO_RV32E = 1"
-}
-
-if { $::env(USE_ZERO_RISCY)==1} {
-    if {$::env(ZERO_RV32E)} {
-          set NAME "MICRO_RISCY"
-          puts "MICRO_RISCY COMPILATION"
-    } else {
-          set NAME "ZERO_RISCY"
-          puts "ZERO_RISCY COMPILATION"
-    }
-} else {
-    if {$::env(RISCY_RV32F)} {
-          set NAME "RISCY_FP"
-          puts "RISCY_FP COMPILATION"
-    } else { 
-          set NAME "RISCY"
-          puts "RISCY COMPILATION"
-    }
-}
-
-
+set NAME "ZERO_RISCY"
 
 # create project
 create_project pulpino . -part $::env(XILINX_PART)
@@ -62,7 +19,7 @@ set_property board $::env(XILINX_BOARD) [current_project]
 source tcl/ips_inc_dirs.tcl
 
 # set up meaningful errors
-source ../common/messages.tcl
+source ../../common/messages.tcl
 
 source tcl/ips_src_files.tcl
 source tcl/src_files.tcl
@@ -73,22 +30,22 @@ add_files -norecurse $FPGA_IPS/xilinx_fp_fma/ip/xilinx_fp_fma.dcp
 
 source ./tcl/ips_add_files.tcl
 if {$::env(USE_ZERO_RISCY)==0} {
- remove_files $SRC_ZERORISCY_REGFILE_FPGA
- remove_files $SRC_ZERORISCY
- add_files -norecurse -scan_for_includes $SRC_RISCV
- add_files -norecurse -scan_for_includes $SRC_RISCV_REGFILE_FPGA
+ remove_files $SRC_IBEX_REGFILE_FPGA
+ remove_files $SRC_IBEX
+ #add_files -norecurse -scan_for_includes $SRC_RISCV
+ #add_files -norecurse -scan_for_includes $SRC_RISCV_REGFILE_FPGA
 } else {
- remove_files $SRC_RISCV_REGFILE_FPGA
- remove_files $SRC_RISCV
- add_files -norecurse -scan_for_includes $SRC_ZERORISCY_REGFILE_FPGA
- add_files -norecurse -scan_for_includes $SRC_ZERORISCY
+ #remove_files $SRC_RISCV_REGFILE_FPGA
+ #remove_files $SRC_RISCV
+ add_files -norecurse -scan_for_includes $SRC_IBEX_REGFILE_FPGA
+ add_files -norecurse -scan_for_includes $SRC_IBEX
 }
 # add components
 add_files -norecurse $SRC_COMPONENTS
 
 # add pulpino
 add_files -norecurse $SRC_PULPINO
-
+add_files -norecurse $SRC_USER_PLUGIN 
 # set pulpino_top as top
 set_property top pulpino [current_fileset]
 
@@ -103,11 +60,11 @@ set_property strategy Flow_AreaOptimized_High [get_runs synth_1]
 
 # run synthesis
 # first try will fail
-catch {synth_design -rtl -name rtl_1 -generic USE_ZERO_RISCY=$::env(USE_ZERO_RISCY) -generic RISCY_RV32F=$::env(RISCY_RV32F) -generic ZERO_RV32M=$::env(ZERO_RV32M) -generic ZERO_RV32E=$::env(ZERO_RV32E) -verilog_define PULP_FPGA_EMUL=1 -verilog_define RISCV -flatten_hierarchy full -gated_clock_conversion on -constrset constrs_1}
+catch {synth_design -rtl -name rtl_1 -generic USE_ZERO_RISCY=$::env(USE_ZERO_RISCY) -verilog_define PULP_FPGA_EMUL=1 -verilog_define RISCV -flatten_hierarchy full -gated_clock_conversion on -constrset constrs_1}
 
 update_compile_order -fileset sources_1
 
-synth_design -rtl -name rtl_1 -generic USE_ZERO_RISCY=$::env(USE_ZERO_RISCY) -generic RISCY_RV32F=$::env(RISCY_RV32F) -generic ZERO_RV32M=$::env(ZERO_RV32M) -generic ZERO_RV32E=$::env(ZERO_RV32E) -verilog_define PULP_FPGA_EMUL=1 -verilog_define RISCV -flatten_hierarchy full -gated_clock_conversion on -constrset constrs_1
+synth_design -rtl -name rtl_1 -generic USE_ZERO_RISCY=$::env(USE_ZERO_RISCY) -verilog_define PULP_FPGA_EMUL=1 -verilog_define RISCV -flatten_hierarchy full -gated_clock_conversion on -constrset constrs_1
 
 #set_property STEPS.SYNTH_DESIGN.ARGS.KEEP_EQUIVALENT_REGISTERS true [get_runs synth_1]
 #set_property STEPS.SYNTH_DESIGN.ARGS.RESOURCE_SHARING off [get_runs synth_1]
---
+++ fpga/xilinx_2015_1.sh
@@ -0,0 +1,6 @@
+export XILINX_HOME=/home/wanjun/vivado/2015.1
+export VIVADO_HOME=$XILINX_HOME/Vivado/2015.1
+export VITIS_HOME=$XILINX_HOME/SDK/2015.1
+export XILINX_VIVADO=/home/wanjun/vivado/2015.1/Vivado/2015.1
+export PATH=$VIVADO_HOME/bin:$VITIS_HOME/bin:$VITIS_HOME/gnu/microblaze/lin/bin:$VITIS_HOME/gnu/arm/lin/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_be/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_le/bin:$XILINX_HOME/DocNav:$PATH
+
