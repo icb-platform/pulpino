44cc21aa01ac448ef39da34218da8b23d7ac202e
DingFu <dfhappyforever@msn.com>
2021-09-23 16:06:57 +0800

    fix XDC

--- fpga/pulpino-icbv1board/rtl/pulpino_wrap.v
+++ fpga/pulpino-icbv1board/rtl/pulpino_wrap.v
@@ -1,10 +1,10 @@
 // Copyright 2017 ETH Zurich and University of Bologna.
 // Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the “License”); you may not use this file except in
+// License, Version 0.51 (the “License�?); you may not use this file except in
 // compliance with the License.  You may obtain a copy of the License at
 // http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
 // or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
+// this License is distributed on an “AS IS�? BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
@@ -16,47 +16,51 @@ module pulpino(
 
   spi_clk_i,
   spi_cs_i,
-  spi_mode_o,
+//  spi_mode_o,
   spi_sdo0_o,
-  spi_sdo1_o,
-  spi_sdo2_o,
-  spi_sdo3_o,
+//  spi_sdo1_o,
+//  spi_sdo2_o,
+//  spi_sdo3_o,
   spi_sdi0_i,
-  spi_sdi1_i,
-  spi_sdi2_i,
-  spi_sdi3_i,
+//  spi_sdi1_i,
+//  spi_sdi2_i,
+//  spi_sdi3_i,
 
   spi_master_clk_o,
   spi_master_csn0_o,
   spi_master_csn1_o,
   spi_master_csn2_o,
   spi_master_csn3_o,
-  spi_master_mode_o,
+//  spi_master_mode_o,
   spi_master_sdo0_o,
-  spi_master_sdo1_o,
-  spi_master_sdo2_o,
-  spi_master_sdo3_o,
+//  spi_master_sdo1_o,
+//  spi_master_sdo2_o,
+//  spi_master_sdo3_o,
   spi_master_sdi0_i,
-  spi_master_sdi1_i,
-  spi_master_sdi2_i,
-  spi_master_sdi3_i,
+//  spi_master_sdi1_i,
+//  spi_master_sdi2_i,
+//  spi_master_sdi3_i,
 
   uart_tx,
   uart_rx,
-  uart_rts,
-  uart_dtr,
-  uart_cts,
-  uart_dsr,
+//  uart_rts,
+//  uart_dtr,
+//  uart_cts,
+//  uart_dsr,
 
+  /*
   scl_i,
   scl_o,
   scl_oen_o,
   sda_i,
   sda_o,
   sda_oen_o,
+  */
+  scl,
+  sda,
 
-  gpio_out,
-  
+  gpio,
+
   tck_i,
   trstn_i,
   tms_i,
@@ -72,48 +76,78 @@ module pulpino(
 
   input         spi_clk_i;
   input         spi_cs_i;
-  output  [1:0] spi_mode_o;
+//  output  [1:0] spi_mode_o;
   output        spi_sdo0_o;
-  output        spi_sdo1_o;
-  output        spi_sdo2_o;
-  output        spi_sdo3_o;
+//  output        spi_sdo1_o;
+//  output        spi_sdo2_o;
+//  output        spi_sdo3_o;
   input         spi_sdi0_i;
-  input         spi_sdi1_i;
-  input         spi_sdi2_i;
-  input         spi_sdi3_i;
+//  input         spi_sdi1_i;
+//  input         spi_sdi2_i;
+//  input         spi_sdi3_i;
 
   output        spi_master_clk_o;
   output        spi_master_csn0_o;
   output        spi_master_csn1_o;
   output        spi_master_csn2_o;
   output        spi_master_csn3_o;
-  output  [1:0] spi_master_mode_o;
+//  output  [1:0] spi_master_mode_o;
   output        spi_master_sdo0_o;
-  output        spi_master_sdo1_o;
-  output        spi_master_sdo2_o;
-  output        spi_master_sdo3_o;
+//  output        spi_master_sdo1_o;
+//  output        spi_master_sdo2_o;
+//  output        spi_master_sdo3_o;
   input         spi_master_sdi0_i;
-  input         spi_master_sdi1_i;
-  input         spi_master_sdi2_i;
-  input         spi_master_sdi3_i;
+//  input         spi_master_sdi1_i;
+//  input         spi_master_sdi2_i;
+//  input         spi_master_sdi3_i;
 
   output        uart_tx;
   input         uart_rx;
-  output        uart_rts;
-  output        uart_dtr;
-  input         uart_cts;
-  input         uart_dsr;
+//  output        uart_rts;
+//  output        uart_dtr;
+//  input         uart_cts;
+//  input         uart_dsr;
 
+/*
   input         scl_i;
   output        scl_o;
   output        scl_oen_o;
   input         sda_i;
   output        sda_o;
   output        sda_oen_o;
+  */
+  inout        scl;
+  inout        sda;
+  
+  inout  [20:0] gpio;
+  
+  //IIC
+  wire         scl_i;
+  wire         scl_o;
+  wire         scl_oen_o;
+  wire         sda_i;
+  wire         sda_o;
+  wire         sda_oen_o;
+  
+  assign       scl_i = scl;
+  assign       scl   = scl_oen_o ? 1'bz : scl_o;
 
-  output   [3:0]  gpio_out;
- // output [31:0] gpio_in;
- // output [31:0] gpio_dir;
+  assign       sda_i = sda;
+  assign       sda   = sda_oen_o ? 1'bz: sda_o;
+  
+  //gpio
+  wire  [20:0] gpio_in;
+  wire  [20:0] gpio_dir;
+  wire  [20:0] gpio_out;
+  //
+  assign gpio_in = gpio;
+  genvar i;
+  generate
+    for (i = 0; i < 20; i = i + 1)
+        begin: gen_gpio
+           assign  gpio[i] = gpio_dir[i] ? gpio_out[i] : 1'bz;
+        end
+  endgenerate
 
   // JTAG signals
   input  tck_i;
@@ -127,30 +161,15 @@ module pulpino(
   parameter ZERO_RV32M = 1;
   parameter ZERO_RV32E = 0;
    
-  wire  [31:0] gpio_in;
-  wire  [31:0] gpio_dir;
-  wire  [31:0] gpio_out_r;
-  
   reg          usr_clk;
-  reg   [25:0] cnt ;
+  //reg   [25:0] cnt ;
   reg   [3:0]  usr_cnt;
   
-  assign gpio_out[2:0] = gpio_out_r[2:0];
-  assign gpio_out[3] = (cnt < 26'd2500_0000) ? 1'b1 : 1'b0 ;
-  always @ (posedge clk or negedge rst_n) begin
-  if(!rst_n)
-    cnt <= 26'd0;
-  else if(cnt < 26'd5000_0000)
-    cnt <= cnt + 1'b1;
-  else
-    cnt <= 26'd0;
-  end
-
   reg [3:0] counter;
   always@(posedge clk or negedge rst_n) begin
   if(!rst_n)
     counter <= 4'd0;
-  else if(counter==4'd4)
+  else if(counter==4'd1)
     counter <= 4'd0;
   else
     counter <= counter + 1'd1;
@@ -159,7 +178,7 @@ module pulpino(
   always@(posedge clk or negedge rst_n) begin
   if(!rst_n)
     usr_clk <= 4'd0;
-  else if(counter==4'd4)
+  else if(counter==4'd1)
     usr_clk <= ~usr_clk;
   else
     usr_clk <= usr_clk;
@@ -182,42 +201,42 @@ module pulpino(
     .clk_standalone_i  ( 1'b0              ),
 
     .testmode_i        ( 1'b0              ),
-    .fetch_enable_i    ( ~fetch_enable_n   ),
+    .fetch_enable_i    ( fetch_enable_n   ),
     .scan_enable_i     ( 1'b0              ),
 
     .spi_clk_i         ( spi_clk_i         ), 
     .spi_cs_i          ( spi_cs_i          ),
-    .spi_mode_o        ( spi_mode_o        ),
+    .spi_mode_o        (                   ),
     .spi_sdo0_o        ( spi_sdo0_o        ),
-    .spi_sdo1_o        ( spi_sdo1_o        ),
-    .spi_sdo2_o        ( spi_sdo2_o        ),
-    .spi_sdo3_o        ( spi_sdo3_o        ),
+    .spi_sdo1_o        (                   ),
+    .spi_sdo2_o        (                   ),
+    .spi_sdo3_o        (                   ),
     .spi_sdi0_i        ( spi_sdi0_i        ),
-    .spi_sdi1_i        ( spi_sdi1_i        ),
-    .spi_sdi2_i        ( spi_sdi2_i        ),
-    .spi_sdi3_i        ( spi_sdi3_i        ),
+    .spi_sdi1_i        ( 1'b0              ),
+    .spi_sdi2_i        ( 1'b0              ),
+    .spi_sdi3_i        ( 1'b0              ),
 
     .spi_master_clk_o  ( spi_master_clk_o  ),
     .spi_master_csn0_o ( spi_master_csn0_o ),
     .spi_master_csn1_o ( spi_master_csn1_o ),
     .spi_master_csn2_o ( spi_master_csn2_o ),
     .spi_master_csn3_o ( spi_master_csn3_o ),
-    .spi_master_mode_o ( spi_master_mode_o ),
+    .spi_master_mode_o (                   ),
     .spi_master_sdo0_o ( spi_master_sdo0_o ),
-    .spi_master_sdo1_o ( spi_master_sdo1_o ),
-    .spi_master_sdo2_o ( spi_master_sdo2_o ),
-    .spi_master_sdo3_o ( spi_master_sdo3_o ),
+    .spi_master_sdo1_o (                   ),
+    .spi_master_sdo2_o (                   ),
+    .spi_master_sdo3_o (                   ),
     .spi_master_sdi0_i ( spi_master_sdi0_i ),
-    .spi_master_sdi1_i ( spi_master_sdi1_i ),
-    .spi_master_sdi2_i ( spi_master_sdi2_i ),
-    .spi_master_sdi3_i ( spi_master_sdi3_i ),
+    .spi_master_sdi1_i ( 1'b0              ),
+    .spi_master_sdi2_i ( 1'b0              ),
+    .spi_master_sdi3_i ( 1'b0              ),
 
     .uart_tx           ( uart_tx           ), // output
     .uart_rx           ( uart_rx           ), // input
-    .uart_rts          ( uart_rts          ), // output
-    .uart_dtr          ( uart_dtr          ), // output
-    .uart_cts          ( uart_cts          ), // input
-    .uart_dsr          ( uart_dsr          ), // input
+    .uart_rts          (                   ), // output
+    .uart_dtr          (                   ), // output
+    .uart_cts          ( 1'b0              ), // input
+    .uart_dsr          ( 1'b0              ), // input
 
     .scl_pad_i         ( scl_i             ),
     .scl_pad_o         ( scl_o             ),
@@ -227,7 +246,7 @@ module pulpino(
     .sda_padoen_o      ( sda_oen_o         ),
 
     .gpio_in           ( gpio_in           ),
-    .gpio_out          ( gpio_out_r        ),
+    .gpio_out          ( gpio_out          ),
     .gpio_dir          ( gpio_dir          ),
     .gpio_padcfg       (                   ),
 
